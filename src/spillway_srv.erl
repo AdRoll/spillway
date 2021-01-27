19fa6dd1aeb6b3600880c22a05733414a0be7ff5
Brujo Benavides <fernando.benavides@adroll.com>
2021-01-27 21:48:25 +0100

    [RTI-8865] Kill dead code with fire 🔥 (#13)
    * [RTI-8865] Kill dead code with fire 🔥
    
    * [RTI-8865] Actually add hank

--- src/spillway_srv.erl
+++ src/spillway_srv.erl
@@ -28,10 +28,8 @@
 %% API
 -export([start_link/0, enter/3, leave/2, cur/1, state/0]).
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
-         code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
 
--define(SERVER, ?MODULE).
 -define(TID, spillway).
 
 -record(counter, {name, value = 0 :: non_neg_integer()}).
@@ -99,7 +97,7 @@ state() ->
 %%%===================================================================
 
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init(_Args) ->
     create_ets(),
@@ -114,13 +112,6 @@ handle_cast(_Msg, State) ->
 
 handle_info(_Info, State) ->
     {noreply, State}.
-
-terminate(_Reason, _State) ->
-    %% note: we own the ets, and it will be automatically deleted when we terminate.
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
 
 %%%% Internal Functions
 create_ets() ->
